4580e50ec5a4c72c6b0e8ed3bfed90b85ac5cbcd
swyner97 <swyner97@gmail.com>
2022-07-18 21:33:24 -0700

    game logic

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+let wins = 0;
+let loses = 0;
+let draws = 0;
+
+let randomOption = ['rock', 'paper', 'scissors'];
+
+let gameLogic = () => {
+    let userChoice = window.prompt('Select rock, paper or scissors');
+    console.log(userChoice);
+
+    let randomIndex = Math.floor(Math.random() * randomOption.length);
+    console.log(randomOption[randomIndex]);
+
+    let cpuChoice = randomOption[randomIndex];
+
+    alert(`The CPU chose: ${cpuChoice}`);
+
+    if (userChoice === cpuChoice) {
+        alert("it's a draw!")
+        draws++
+    } else if ((userChoice === randomOption[0] && cpuChoice === randomOption[1]) ||
+        (userChoice === randomOption[1] && cpuChoice === randomOption[2]) || (userChoice === randomOption[2] && cpuCHoice === randomOption[0]))
+    {
+        alert("CPU wins!")
+        loses++
+    } else {
+        alert("You win!")
+        wins++
+    }
+
+};
+
+gameLogic();
